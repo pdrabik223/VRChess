24aed016b5bb036768cafd7e0ad64f4a1c0c557e
piotr233 <pdrabik223@gmail.com>
2022-04-06 21:22:11 +0200

    Added FastLED library with demo script

--- Arduino-Api/src/main.cpp
+++ Arduino-Api/src/main.cpp
@@ -1,9 +1,36 @@
-#include <Arduino.h>
+#include <FastLED.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// How many leds in your strip?
+#define NUM_LEDS 3
+#define DATA_PIN 5
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+void setup()
+{
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS); // GRB ordering is assumed
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  leds[0] = CRGB::AliceBlue;
+  FastLED.show();
+  delay(500);
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(500);
+  leds[1] = CRGB::AliceBlue;
+  FastLED.show();
+  delay(500);
+  leds[1] = CRGB::Black;
+  FastLED.show();
+  delay(500);
+
+  leds[2] = CRGB::AliceBlue;
+  FastLED.show();
+  delay(500);
+  leds[2] = CRGB::Black;
+  FastLED.show();
+  delay(500);
 }
